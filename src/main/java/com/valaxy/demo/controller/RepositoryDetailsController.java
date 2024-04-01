5cb49e417eb53b442c6d4d76cb22c6e51ebaade7
weyewweyew <weyewweyew@gmail.com>
2024-03-31 20:57:03 -0400

    web feature

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Liverpool FC";
+		return "Liverpool FC Top of the League";
 	}
 
 	@GetMapping("/trends")
